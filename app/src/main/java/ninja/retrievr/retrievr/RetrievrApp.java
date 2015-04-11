3627ecca4e9566a6ce9476bc4fb4129ca1e54940
Matt McKenna <matt.c.mckenna@gmail.com>
2015-04-11 02:48:23 -0400

    Added Parse App ID and Client Key to Application Class

--- app/src/main/java/ninja/retrievr/retrievr/RetrievrApp.java
+++ app/src/main/java/ninja/retrievr/retrievr/RetrievrApp.java
@@ -2,13 +2,18 @@ package ninja.retrievr.retrievr;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
 
 /**
  * Created by mattmckenna on 4/11/15.
  */
 public class RetrievrApp extends Application {
 
-    public void onCreate(){
+    public void onCreate() {
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
 
+        Parse.initialize(this, "326Ce5L5lxxfMFHbfa1wE7OFLvphQ2FC2MxeJlWW", "PftH2OrYoAwvR1XcHrZxLLGGWIEQBqT2mDelM6oY");
     }
 }
